07da18e0b71ce60517cb49e307afca3d9ae2cd87
celebi19 <hakan_socialmedia@hotmail.com>
2021-08-08 17:54:11 -0700

    this is my first commit

---
+++ python-for-beginners/HakanDeneme/sentence = Bugun hava guzel.py
@@ -0,0 +1,8 @@
+sentence = "Bugun hava guzel"
+print(sentence)
+print(sentence.capitalize())
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.count("a"))
+print(sentence.count("g"))
+print(sentence.isprintable())
